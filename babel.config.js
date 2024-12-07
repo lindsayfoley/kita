7a9406a3edc03f215bb7f6c30a4eb17eede3ab65
Lindsay Foley <lindsay.foley@outlook.com>
2024-12-07 19:19:28 +0000

    fixing JSX runtime

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   presets: [
+    [
+      "@babel/preset-react",
+      {
+        runtime: "automatic",
+      },
+    ],
     "@babel/preset-env",
-    "@babel/preset-react",
     "@babel/preset-typescript",
   ],
 };
