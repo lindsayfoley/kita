a8d4e36f88121b908c212589bf1133e937ac1a65
Lindsay Foley <lindsay.foley@outlook.com>
2024-12-07 19:20:31 +0000

    adding new components to homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,9 @@
 import Head from "next/head";
-import styles from "@kita/styles/Home.module.css";
+import { Card } from "@kita/components/molecules/Card";
+import { Icon } from "@kita/components/atoms/Icon";
+import { defaultStoryList } from "@kita/components/molecules/Card/mockdata";
+import { IconListGroup } from "@kita/components/molecules/IconListGroup";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -9,9 +13,20 @@ export default function Home() {
         <meta name="description" content="Lindsay's Kita test submission" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <div className={styles.page}>
-        <main className={styles.main}>
-          <aside></aside>
+      <div>
+        <main>
+          <aside className={styles.aside}>
+            <Card
+              theme={Card.Theme.dark}
+              header={
+                <>
+                  <h4>Lesson 3: Morse Code</h4> <Icon name="plus" />
+                </>
+              }
+              body={<IconListGroup list={defaultStoryList} />}
+              borderRadiusSize={Card.BorderRadiusSize.small}
+            />
+          </aside>
         </main>
       </div>
     </>
