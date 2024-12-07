efc7202e4cfc3a16b049dd83b13917e3a3446272
Lindsay Foley <lindsay.foley@outlook.com>
2024-12-07 17:42:27 +0000

    adding Python icon and React imports

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,11 +1,13 @@
 import DocumentText from "./DocumentText";
 import Ellipsis from "./Ellipsis";
 import MagnifyingGlass from "./MagnifyingGlass";
+import PythonColoured from "./PythonColoured";
 
 const icons = {
   magnifyingGlass: MagnifyingGlass,
   ellipsis: Ellipsis,
   documentText: DocumentText,
+  pythonColoured: PythonColoured,
 };
 
 export default icons;
