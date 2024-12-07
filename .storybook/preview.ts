e96c54ace3130d45c6a86ec7fe6955481d37efc4
Lindsay Foley <lindsay.foley@outlook.com>
2024-12-07 18:56:48 +0000

    storing colours in vars

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,5 @@
 import type { Preview } from "@storybook/react";
+import "../src/styles/colours.css";
 
 const preview: Preview = {
   parameters: {
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import "@kita/styles/globals.css";
+import "@kita/styles/colours.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
